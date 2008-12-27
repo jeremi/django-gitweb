421622254c78d5714ad5fa349f0a4496062531df
Stephan Jaekel <steph@rdev.info>
2008-12-27 22:03:50 +0100

    fixed typo

--- example_project/urls.py
+++ example_project/urls.py
@@ -5,13 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', include('gitweb.urls'))
+    (r'^', include('gitweb.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,'show_indexes': True }),
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
-    
